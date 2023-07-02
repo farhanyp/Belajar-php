14277055d38b6788f30591b08ea048cd8cb44cea
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-07-02 12:04:04 +0700

    Desctructor

---
+++ PHP-OOP/destructor/class/Person.php
@@ -0,0 +1,38 @@
+<?php
+
+// Membuat Class
+class Person{
+
+    const VERSION = "farhan yp";
+
+    // Membuat Class, menambahkan types declaration, menentukan default value dan nullable
+    var string $name;
+    var ?string $address;
+    var string $country = "indonesia";
+
+
+    // Membuat Constructor
+    function __construct(string $name, ?string $address){
+        $this->name = $name;
+        $this->address = $address;
+    }
+
+    function sayHello (?string $name): string{
+
+        if(is_null($name)){
+            return "My name is {$this->$name}";
+        }else{
+            return "Hello {$name}, my name is {$this->name}";
+        }
+    }
+
+    // Memanggil didalam class itu sendiri dan diletakan didalam method atau function
+    function info(){
+        return self::VERSION;
+    }
+
+    // Membuat function desctructor yang berguna untuk jika aplikasi selesai maka function ini akan dipanggil
+    function __destruct(){
+        echo "This Person {$this->name} is Destroyed".PHP_EOL;
+    }
+}
---
+++ PHP-OOP/destructor/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "destructor/class/Person.php";
+
+// Memanggil class yang memiliki constructor
+$yp = new Person("Yp", null);
+$farhan = new Person("Farhan", null);
+
+echo("Aplikasi selesai".PHP_EOL);
