72e8d000fd6a2853f06d310ee07103a55fc33e08
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-07-09 04:29:50 +0700

    Membuat project composer

---
+++ PHP-Composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yp/belajar-php-composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aa9c454d143793dd410cb4160acc67689be53ee6',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yp/belajar-php-composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aa9c454d143793dd410cb4160acc67689be53ee6',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
