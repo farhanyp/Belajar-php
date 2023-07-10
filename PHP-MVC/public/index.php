1961e2a29209122397864b84f834aac7c521c4b3
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-07-11 05:42:45 +0700

    PHP MVC

--- PHP-MVC/public/index.php
+++ PHP-MVC/public/index.php
@@ -1,16 +1,17 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use BelajarPhpMvc\Belajar\PHP\MVC\App\Router;
-use BelajarPhpMvc\Belajar\PHP\MVC\Controller\HomeController;
-use BelajarPhpMvc\Belajar\PHP\MVC\Controller\ProductController;
-use BelajarPhpMvc\Belajar\PHP\MVC\Middleware\AuthMiddleware;
+use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\ProductController;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\AuthMiddleware;
 
-Router::add("GET", "/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)", ProductController::class,"categories");
+Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
 
-Router::add("GET", "/", HomeController::class, "index");
-Router::add("GET", "/home", HomeController::class, "login", [AuthMiddleware::class]);
-Router::add("GET", "/about", HomeController::class, "about", [AuthMiddleware::class]);
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
+Router::add('GET', '/world', HomeController::class, 'world', [AuthMiddleware::class]);
+Router::add('GET', '/about', HomeController::class, 'about');
 
 Router::run();
--- PHP-MVC/test/RegexTest.php
+++ PHP-MVC/test/RegexTest.php
@@ -1,19 +1,27 @@
 <?php
 
-use PHPUnit\Framework\Assert;
+namespace ProgrammerZamanNow\Belajar\PHP\MVC;
+
 use PHPUnit\Framework\TestCase;
 
-class RegexTest extends TestCase{
+class RegexTest extends TestCase
+{
+    public function testRegex()
+    {
 
-    public function testRegex(){
         $path = "/products/12345/categories/abcde";
 
         $pattern = "#^/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)$#";
 
         $result = preg_match($pattern, $path, $variables);
 
-        Assert::assertEquals(1, $result);
+        self::assertEquals(1, $result);
 
         var_dump($variables);
+
+        array_shift($variables);
+        var_dump($variables);
+
     }
+
 }
